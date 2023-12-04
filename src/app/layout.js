294e528e71660464915765349f40c2521dcd38c6
Adarsh Chaluvaraju <cadarsh335@gmail.com>
2023-12-04 00:08:10 -0500

    [Add] Navigation Bar and cleaning bloat

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,8 @@
-import { Inter } from 'next/font/google'
+import { Inter, IBM_Plex_Mono, IBM_Plex_Sans } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const ibmPlexMono = IBM_Plex_Mono({ subsets: ['latin'], weight: ['400', '500', '700'] })
+const ibmPlexSans = IBM_Plex_Sans({ subsets: ['latin'], weight: ['400', '700'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +12,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${ibmPlexSans.className}`}>
+        <div className="navbar">
+          <div className='logo'>SAMOYEDS</div>
+          <div className={`${ibmPlexMono.className} nav-elements`}>
+            <span className='nav-element'>Dashboard</span>
+            <span className='nav-element'>Projects</span>
+            <span className='nav-element'>About</span>
+            <span className='nav-element'>Safety</span>
+            <span className='nav-element cta'>Start Simulation</span>
+            <span className='nav-element avatar'>
+              <img src="./user-avatar.png" alt="Avatar" />
+            </span> 
+          </div>
+        </div>
+        {children}</body>
     </html>
   )
 }
